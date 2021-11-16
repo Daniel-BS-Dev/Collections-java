6152c90fc1c470dca16ed76710f846fff3e909c9
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-16 13:24:12 -0300

    list

---
+++ List/dio.com/src/dio/com/comparator/ExempleList.java
@@ -0,0 +1,49 @@
+package dio.com.comparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ExempleList {
+	
+	public static void main(String[] args) {
+	
+	List<String> names = new ArrayList<>() {{
+		add("Daniel");
+		add("Alana");
+		add("Maria");
+		add("Felipe");
+		add("Jeferson");
+	}};
+	
+	System.out.println(names);
+	
+	//order alfabetica
+	Collections.sort(names);
+	System.out.println(names);
+	
+	//atualizar name
+	names.set(3, "daro");
+	System.out.println(names);
+	
+	//removendo o name daro
+	names.remove(3);
+	System.out.println(names);
+	
+	//tamanho da list
+	System.out.println(names.size());
+	
+	//pegando a posição do elemento. Se não tem ele returna -1
+	System.out.println(names.indexOf("Paulo"));
+	
+	//pegando um valor
+	System.out.println(names.get(2));
+	
+	//contem elemento na list
+	System.out.println(names.contains("Paulo"));
+	
+	//limpando a list
+	names.clear();
+	System.out.println(names);
+	}
+}
